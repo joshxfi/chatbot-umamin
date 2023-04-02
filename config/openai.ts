6d5648355aa21b9e889db45f799389d28f263870
Josh Daniel <joshxfi.dev@gmail.com>
2023-04-02 15:33:12 +0800

    feat: setup openai

---
+++ config/openai.ts
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+export default openai;
--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,10 +1,18 @@
 import { Query, Resolver } from "type-graphql";
+import openai from "../config/openai";
 import { Hello } from "./types";
 
 @Resolver(Hello)
 export class HelloResolver {
   @Query(() => Hello)
-  hello() {
-    return { message: "Hello world!" };
+  async hello() {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      max_tokens: 7,
+      temperature: 0,
+    });
+
+    return { message: response.data.choices[0].text };
   }
 }
